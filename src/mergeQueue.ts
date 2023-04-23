GiteaBot/gitea-backporter

commit 67d520231c24acf7b81a10c8a1982cef1671801d

Fix conflict detection logic (#53)
- Mention author
- Fix comment log
- Fix merge conflict detection (missed `break`), fixes #52

src/mergeQueue.ts
@@ -10,19 +10,22 @@ export const run = async () => {
   const pendingMerge = await fetchPendingMerge();
 
   // group PRs by milestone
-  const milestoneToPr = new Map<string, number[]>();
+  const milestoneToPr = new Map<
+    string,
+    { number: number; user: { login: string } }[]
+  >();
   for (const pr of pendingMerge.items) {
     const milestone = pr.milestone?.title;
     const prs = milestoneToPr.get(milestone) ?? [];
-    prs.push(pr.number);
+    prs.push(pr);
     milestoneToPr.set(milestone, prs);
   }
 
   // for each milestone, try to update the lowest PR number (only if it needs an update), if it fails, try the next one
   for (const [_, prs] of milestoneToPr) {
     for (const pr of prs) {
-      if (!await needsUpdate(pr)) break;
-      const response = await updatePr(pr);
+      if (!await needsUpdate(pr.number)) break;
+      const response = await updatePr(pr.number);
       if (response.ok) {
         console.info(`Synced PR #${pr} in merge queue`);
         break;
@@ -32,13 +35,17 @@ export const run = async () => {
       if (body.message !== "merge conflict between base and head") {
         console.error(`Failed to sync PR #${pr} in merge queue`);
         console.error(JSON.stringify(body));
+        break;
       }
 
       console.info(`Merge conflict detected in PR #${pr} in merge queue`);
       // if there is a merge conflict, we'll add a comment to fix the conflicts and remove the reviewed/wait-merge label
       await Promise.all([
-        addPrComment(pr, "Please fix the merge conflicts. :tea:"),
-        removeLabel(pr, "reviewed/wait-merge"),
+        addPrComment(
+          pr.number,
+          `@${pr.user.login} please fix the merge conflicts. :tea:`,
+        ),
+        removeLabel(pr.number, "reviewed/wait-merge"),
       ]);
     }
   }

src/github.ts
@@ -324,7 +324,5 @@ export const addPrComment = async (prNumber: number, comment: string) => {
     },
   );
   await response.json();
-  console.log(
-    `Added backport comment to PR #${prNumber}`,
-  );
+  console.info(`Added comment to PR #${prNumber}`);
 };

commit bbf287727e526ef5691f71de1f8a6234d716300e

Fix merge queue message logging
Regression from #53

Signed-off-by: Yarden Shoham <git@yardenshoham.com>

src/mergeQueue.ts
@@ -27,18 +27,20 @@ export const run = async () => {
       if (!await needsUpdate(pr.number)) break;
       const response = await updatePr(pr.number);
       if (response.ok) {
-        console.info(`Synced PR #${pr} in merge queue`);
+        console.info(`Synced PR #${pr.number} in merge queue`);
         break;
       }
 
       const body = await response.json();
       if (body.message !== "merge conflict between base and head") {
-        console.error(`Failed to sync PR #${pr} in merge queue`);
+        console.error(`Failed to sync PR #${pr.number} in merge queue`);
         console.error(JSON.stringify(body));
         break;
       }
 
-      console.info(`Merge conflict detected in PR #${pr} in merge queue`);
+      console.info(
+        `Merge conflict detected in PR #${pr.number} in merge queue`,
+      );
       // if there is a merge conflict, we'll add a comment to fix the conflicts and remove the reviewed/wait-merge label
       await Promise.all([
         addPrComment(