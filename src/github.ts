GiteaBot/gitea-backporter

commit 4d71465d6331ec4504e82fbeaafc7e76fd15a3ef

Refactor to reuse approver calculation in backport review request and LGTM (#59)
Now we have only one implementation counting reviews

src/github.ts
@@ -116,51 +116,6 @@ export const setCommitStatus = (
   );
 };
 
-// returns the number of approvals a PR has
-export const getPrApprovalNumber = async (
-  prNumber: number,
-): Promise<number> => {
-  // load all reviews
-  const reviews: {
-    state:
-      | "APPROVED"
-      | "CHANGES_REQUESTED"
-      | "COMMENTED"
-      | "DISMISSED"
-      | "PENDING";
-    user: { login: string };
-  }[] = [];
-  let page = 1;
-  while (true) {
-    const response = await fetch(
-      `${GITHUB_API}/repos/go-gitea/gitea/pulls/${prNumber}/reviews?per_page=100&page=${page}`,
-      { headers: HEADERS },
-    );
-    if (!response.ok) throw new Error(await response.text());
-    const results: [] = await response.json();
-    if (results.length === 0) break;
-    reviews.push(...results);
-    page++;
-  }
-
-  // count approvers by replaying all reviews (they are already sorted)
-  const approvers = new Set();
-  for (const review of reviews) {
-    switch (review.state) {
-      case "APPROVED":
-        approvers.add(review.user.login);
-        break;
-      case "DISMISSED":
-      case "CHANGES_REQUESTED":
-        approvers.delete(review.user.login);
-        break;
-      default:
-        break;
-    }
-  }
-  return approvers.size;
-};
-
 // get a go-gitea/gitea branch
 export const fetchBranch = async (branch: string) => {
   const response = await fetch(
@@ -266,18 +221,46 @@ export const getMilestones = async (): Promise<Milestone[]> => {
 };
 
 export const getPrApprovers = async (prNumber: number) => {
-  const response = await fetch(
-    `${GITHUB_API}/repos/go-gitea/gitea/pulls/${prNumber}/reviews`,
-    { headers: HEADERS },
-  );
-  const json = await response.json();
+  // load all reviews
+  const reviews: {
+    state:
+      | "APPROVED"
+      | "CHANGES_REQUESTED"
+      | "COMMENTED"
+      | "DISMISSED"
+      | "PENDING";
+    user: { login: string };
+  }[] = [];
+  let page = 1;
+  while (true) {
+    const response = await fetch(
+      `${GITHUB_API}/repos/go-gitea/gitea/pulls/${prNumber}/reviews?per_page=100&page=${page}`,
+      { headers: HEADERS },
+    );
+    if (!response.ok) throw new Error(await response.text());
+    const results: [] = await response.json();
+    if (results.length === 0) break;
+    reviews.push(...results);
+    page++;
+  }
 
-  const approvers = json
-    .filter((review: { state: string }) => review.state === "APPROVED")
-    .map((r: { user: { login: string } }) => r.user.login);
+  // count approvers by replaying all reviews (they are already sorted)
+  const approvers = new Set<string>();
+  for (const review of reviews) {
+    switch (review.state) {
+      case "APPROVED":
+        approvers.add(review.user.login);
+        break;
+      case "DISMISSED":
+      case "CHANGES_REQUESTED":
+        approvers.delete(review.user.login);
+        break;
+      default:
+        break;
+    }
+  }
 
-  // return unique approvers
-  return [...new Set(approvers)];
+  return approvers;
 };
 
 export const createBackportPr = async (

src/github_test.ts
@@ -1,29 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.184.0/testing/asserts.ts";
-import { fetchBranch, getPrApprovalNumber, getPrApprovers } from "./github.ts";
+import { fetchBranch, getPrApprovers } from "./github.ts";
 
 Deno.test("getPrApprovers() returns the appropriate approvers", async () => {
   const prToApprovers = {
-    23993: ["delvh", "jolheiser"],
-    24051: ["delvh", "silverwind"],
-    24047: ["yardenshoham", "lunny"],
+    23993: new Set(["delvh", "jolheiser"]),
+    24051: new Set(["delvh", "silverwind"]),
+    24047: new Set(["yardenshoham", "lunny"]),
+    24055: new Set<string>(),
+    24254: new Set(["jolheiser", "yardenshoham"]),
+    24259: new Set(["jolheiser", "delvh"]),
+    24270: new Set(["lunny", "delvh", "silverwind"]),
   };
   await Promise.all(
     Object.entries(prToApprovers).map(async ([pr, approvers]) => {
       assertEquals(await getPrApprovers(Number(pr)), approvers);
-    }),
-  );
-});
-
-Deno.test("getPrApprovalNumber() returns the appropriate approval number", async () => {
-  const prToNumber = {
-    24270: 3,
-    24254: 2,
-    24259: 2,
-    24055: 0,
-  };
-  await Promise.all(
-    Object.entries(prToNumber).map(async ([pr, number]) => {
-      assertEquals(await getPrApprovalNumber(Number(pr)), number);
     }),
   );
 });

src/lgtm.ts
@@ -1,6 +1,6 @@
 import {
   addLabels,
-  getPrApprovalNumber,
+  getPrApprovers,
   removeLabel,
   setCommitStatus,
 } from "./github.ts";
@@ -14,15 +14,15 @@ export const setPrStatusAndLabel = async (
     number: number;
   },
 ) => {
-  let approvals;
+  let approvers;
   try {
-    approvals = await getPrApprovalNumber(pr.number);
+    approvers = await getPrApprovers(pr.number);
   } catch (error) {
     console.error(error);
     return;
   }
 
-  const { state, message, desiredLabel } = getPrStatusAndLabel(approvals);
+  const { state, message, desiredLabel } = getPrStatusAndLabel(approvers.size);
   const currentLgtmLabels = pr.labels.filter((l) => l.name.startsWith("lgtm/"));
 
   // remove any undesired lgtm labels

commit 9d90e1c527ce7023396ca3fe7513576356fef349

Send an array to GitHub, not a set
Regression from #59

Signed-off-by: Yarden Shoham <git@yardenshoham.com>

src/github.ts
@@ -337,7 +337,7 @@ export const createBackportPr = async (
     {
       method: "POST",
       headers: HEADERS,
-      body: JSON.stringify({ reviewers: approvers }),
+      body: JSON.stringify({ reviewers: [...approvers] }),
     },
   );
 